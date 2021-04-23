7ac48e9c7de3465d760c0285f2cfd1867f756e5a
ZMao <27260660+Charlotte-23@users.noreply.github.com>
2021-04-22 23:11:44 -0400

    'className' instead of 'class'

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -4,7 +4,7 @@ import { Spinner } from 'react-bootstrap'
 const Loader = () => {
     return (
         <Spinner animation='border' role='status' style={{width:100, height:100, margin:'auto', display:'block'}}>
-            <span class='sr-only'>Loading...</span>
+            <span className='sr-only'>Loading...</span>
         </Spinner>
     )
 }
