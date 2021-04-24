d767e4c47899b2edb1bfdc51cc84b91b73311d24
ZMao <27260660+Charlotte-23@users.noreply.github.com>
2021-04-23 21:13:10 -0400

    remove item reducer

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,9 +1,10 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
+    const item = action.payload
     switch (action.type) {
         case CART_ADD_ITEM:
-            const item = action.payload
+            
             const existItem = state.cartItems.find(x => x.product === item.product)
             if (existItem) {
                 return {
@@ -16,6 +17,12 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
                     ...state,
                     cartItems: [...state.cartItems, item],
                 }
+            }
+
+        case CART_REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(x => x.product !== item)
             }
 
         default:
